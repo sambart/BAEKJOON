e0853661b419de382350a372da289e52dd394039
dylan <dylan@walkbrain.com>
2020-07-31 15:31:16 +0900

    test

---
+++ 3015/3015.cpp
@@ -0,0 +1,13 @@
+﻿// 3015.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+}
